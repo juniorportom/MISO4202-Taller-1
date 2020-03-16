c46b37a8b99d6416cdb8ad1ec67eb6fc6d0e0a3b
Reinaldo Junior Portocarrero Marciales <rj.portocarrero281@sis.virtual.uniandes.edu.co>
2020-03-15 20:58:28 -0500

    Se agregan sentias para generarcion de modelos m2t

--- Cuarta Entrega/basics.tf
+++ Cuarta Entrega/basics.tf
@@ -5,72 +5,71 @@
 	
 
 # connection to AWS
-provider "[aws]" {
+provider "[]" {
   profile    = "default"
   region     = "us-east-1"
 }
 #end connection to AWS
 
-
 		
 # create the VPC
-resource "aws_vpc" "[vpc1]" {
-  cidr_block           = [10.0.0.0/16]
+resource "aws_vpc" "vpcdesarrollo" {
+  cidr_block           = 172.34.0.0/16
   instance_tenancy     = "default" 
   enable_dns_support   = true 
   enable_dns_hostnames = true
   
 } 
 # end VPC
-		
+
 		
 # create the security resource
-resource "aws_internet_gateway" "gateway1" {
-  vpc_id = vpc1
+resource "aws_Subred 1" "subred1" {
+  vpc_id = vpcdesarrollo
+  cidr_block              = 172.34.2.0/24
+  map_public_ip_on_launch = true 
+  availability_zone       = z1		
 }
 # end security resouce
 		
 		
 # create the security resource
-resource "aws_subnet" "subnet1" {
-  vpc_id = vpc1
-  cidr_block              = 10.0.1.0/24
+resource "aws_Subred 2" "subred2" {
+  vpc_id = vpcdesarrollo
+  cidr_block              = 172.34.1.0/24
   map_public_ip_on_launch = true 
-  availability_zone       = us-east-1a		
+  availability_zone       = z1		
 }
 # end security resouce
 		
 		
 # create the security resource
-resource "aws_subnet" "subnet2" {
-  vpc_id = vpc1
-  cidr_block              = 10.0.2.0/24
+resource "aws_InternetGateway" "gatewaydesarrollo" {
+  vpc_id = vpcdesarrollo
+  name         = InternetGateway
+  description  = Seguridad desarrollo
   map_public_ip_on_launch = true 
-  availability_zone       = us-east-1b		
-}
-# end security resouce
-		
-		
-# create the security resource
-resource "aws_security_group" "securitygroup1" {
-  vpc_id = vpc1
-  name         = security_group
-  description  = Security Group 1
-  map_public_ip_on_launch = true 
-  availability_zone       = us-east-1a	
+  availability_zone       = z1	
   
   		
  egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = tcp
+    from_port   = 127.0.0.1/0
+    to_port     = 80
+    protocol    = TCP
   }  		
   		
   		
  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
+    from_port   = 127.0.0.1/0
+    to_port     = 8080
+    protocol    = TCP
+  }  		
+  		
+  		
+ egress {
+    from_port   = 192.0.0.1/128
+    to_port     = 22
+    protocol    = TCP
   }  		
   		
   	
@@ -78,25 +77,13 @@ resource "aws_security_group" "securitygroup1" {
 # end security resouce
 		
 
-
-
-
-
-
-
-
 	
-
-
-#Public key
-resource "aws_key_pair" "ec2key" {
-  key_name = "ec2key1"
-  public_key = [[org.eclipse.emf.ecore.impl.DynamicEObjectImpl@3f8ee80f (eClass: org.eclipse.emf.ecore.impl.EClassImpl@5b34e656 (name: AutenticacionFirma) (instanceClassName: null) (abstract: false, interface: false))]]
-}
+	#Public key
+	resource "aws_key_pair" "ec2key" {
+  	key_name = "ec2key1"
+  	public_key = public_key = file("[/home/ubuntu/Documentos/Terraform/terraformtest_key.pem]")
+	}
 #end Public key
-
-
-
 
 
 
@@ -120,65 +107,51 @@ data "aws_ami" "ubuntu" {
 
 
 
-
-
-#Server instance
-resource "aws_instance" "ec21" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.ec2key.key_name
-  vpc_security_group_ids = [aws_security_group.securitygroup1.id]
-  subnet_id = aws_subnet.subnet1.id
+	
+	  #Server instance
+	  resource "aws_instance" "servidorApp" {
+	  ami           = data.aws_ami.ubuntu.id
+	  instance_type = "t2.micro"
+	  key_name = aws_key_pair.ec2key.key_name
+	  vpc_security_group_ids = VPC
+	  subnet_id = aws_subnet.subnet1.id
 
 }
-#End server instane
-
-
-
+#End server instance
+	
 
 
 resource "aws_db_subnet_group" "dbsubnet1" {
   name       = "dbsubnet1"
-  subnet_ids = [aws_subnet.subnet1.id,aws_subnet.subnet2.id]
+  subnet_ids = [subred1,subred2]
 }
 
-
-
-
 	
-
-
-#DB instance
-resource "aws_db_instance" "db1" {
+	
+	#DB instance
+resource "aws_db_instance" "dbdesarrollo" {
   name = "db1"
   allocated_storage    = 20
   storage_type         = "gp2"
-  engine               = "mysql"
+  engine               = "MySql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   username             = "root"
   password             = "miso4202-202002"
   parameter_group_name = "default.mysql5.7"
-  vpc_security_group_ids = [aws_security_group.securitygroup1.id]
+  vpc_security_group_ids = [vpcdesarrollo]
   db_subnet_group_name = aws_db_subnet_group.dbsubnet1.name
 }
 #End db instance
-
-
-
-
-
-
+	
+	
 
 #EBS instance
-resource "aws_ebs_volume" "ebs1" {
-  availability_zone = var.availabilityZone
-  size              = 40
+resource "aws_ebs_volume" "almacenamientodesarrollo" {
+  availability_zone = z1
+  size              = 20.0
 }
 #end EBS instance
-
-
-
 
 
 
@@ -186,11 +159,9 @@ resource "aws_ebs_volume" "ebs1" {
 #EBS attachment
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.ebs1.id
-  instance_id = aws_instance.ec21.id
+  volume_id   = almacenamientodesarrollo
+  instance_id = servidorApp
 }
-
-
 
 
 	
